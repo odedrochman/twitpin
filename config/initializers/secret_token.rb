a139546aa4a36a2f0d23967d74f180910e0d65e0
Oded Rochman <oded.rochman@gmail.com>
2013-07-30 16:36:47 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twitpin::Application.config.secret_token = '339826a0289998d092ac804989bc563e986cd0d0d9dc9c341d9b2d00de947f4473f421ab626bae691a044948463de155a45a8d3b35968afae9e66c065a733870'
